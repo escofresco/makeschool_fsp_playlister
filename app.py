7752047e7be9ed5ce7c80a0bded057205bc4c159
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-26 12:39:11 -0700

    add mongo setup

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import (Flask,
                    render_template)
+from pymongo import MongoClient
 
+# Flask simple setup
 app = Flask(__name__)
 app.config['ENV'] = 'development'
+
+# Mongo setup
+client = MongoClient()
+playlister_db = client.Playlister
+playlist_collection = playlister_db.playlists
 
 @app.route('/')
 def playlists_view():
     """Show all playlists."""
     return render_template('playlists_view.html',
-                           playlist=[{
-                             'title': 'Cat Videos',
-                             'description': 'Cats acting weird'
-                            },
-                            {
-                                'title': '80\'s Music',
-                                'description': 'Don\'t stop believing!'
-                            }])
+                           playlist=playlist_collection.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
