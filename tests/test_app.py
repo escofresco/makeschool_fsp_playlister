4ffce8ad0966cd8f81903ea7fbbf27fcb7a6cada
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-01 12:23:02 -0700

    add view test and new test

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,5 +6,17 @@ class AppTestSuite(unittest.TestCase):
         self.client = app.test_client()
         app.config["TESTING"] = True
 
+    def test_view(self):
+        """Test the playlists homepage."""
+        result = self.client.get('/')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Playlist', result.data)
+
+    def test_new(self):
+        """Test the new playlist creation page."""
+        result = self.client.get('/playlists/new')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'New Playlist', result.data)
+
 if __name__ == "__main__":
     unittest.main()
