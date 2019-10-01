c0820ee0f942278e3064a0e82a0339bff116023d
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-01 12:07:00 -0700

    fix incorrect route for playlists_edit

--- app.py
+++ app.py
@@ -36,11 +36,11 @@ def playlists_show(playlist_id):
         })
     return render_template("playlists_show.html", playlist=playlist_document)
 
-@app.route("/playlists/<playlist_id>")
-def playlists_show(playlist_id):
+@app.route("/playlists/<playlist_id>/edit")
+def playlists_edit(playlist_id):
     """Render edit template for playlist with playlist_id"""
     playlist_document = playlist_collection.find_one({"_id": ObjectId(playlist_id)})
-    return render_template("playlists_edit.html", playlist=playlist_collection)
+    return render_template("playlists_edit.html", playlist=playlist_document)
 
 @app.route("/playlists/new")
 def playlists_new():
