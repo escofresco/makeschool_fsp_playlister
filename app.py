440aad32d248dc92b6e1ec6083f4a50e23f5e30b
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-01 13:09:01 -0700

    fix retry writes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #app.config["ENV"] = "development"
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
